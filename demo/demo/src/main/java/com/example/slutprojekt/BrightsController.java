3ef6ecd9aa7d2d093c9bf7668bdbd148a65213d8
Johan Mattsson <mattsson.cj@gmail.com>
2022-10-12 15:56:56 +0200

    Profile page, yes

--- demo/demo/src/main/java/com/example/slutprojekt/BrightsController.java
+++ demo/demo/src/main/java/com/example/slutprojekt/BrightsController.java
@@ -2,13 +2,21 @@ package com.example.slutprojekt;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.sql.Date;
 import java.util.List;
 
 @Controller
 public class BrightsController {
 
+    @Autowired
+    private StudentRepo studentRepo;
 
 
     /*@GetMapping("/")
@@ -22,14 +30,46 @@ public class BrightsController {
     }
 
     @GetMapping("/profile")
-    public String profile() {
+    public String profile(HttpSession session, HttpServletRequest request, Model model) {
+        String email = request.getRemoteUser();
+
+        for (Student student : studentRepo.findAll()) {
+            if (student.getEmail().equals(email)) {
+                model.addAttribute("student", student);
+            }
+        }
         return "profile";
     }
 
-    @GetMapping("/people")
+    @PostMapping("/profile")
+    public String profilePost(@ModelAttribute Student student, HttpSession session, Model model) {
+        student.setFirstName((String)model.getAttribute("firstName"));
+        student.setLastName((String)model.getAttribute("lastName"));
+        student.setEmail((String)model.getAttribute("email"));
+        student.setLinkedIn((String)model.getAttribute("linkedIn"));
+        student.setGitHub((String)model.getAttribute("gitHub"));
+        //student.setUsername((String)session.getAttribute("username"));
+        /*student.setFirstName((String)session.getAttribute("firstName"));
+        student.setLastName((String)session.getAttribute("lastName"));
+        student.setEmail((String)session.getAttribute("email"));
+        student.setLinkedIn((String)session.getAttribute("linkedIn"));
+        student.setGitHub((String)session.getAttribute("gitHub"));*/
+        //student.setCity((City)session.getAttribute("city"));
+        //student.setAddress((String)session.getAttribute("address"));
+        //student.setDateOfBirth((Date)session.getAttribute("dateofBirth"));
+
+        //student.setPassword((String)session.getAttribute("password"));
+        studentRepo.save(student);
+
+
+        //return "redirect:/profile";
+        return "";
+    }
+
+    /*@GetMapping("/people")
     public  String people() {
         return "people";
-    }
+    }*/
 
     @GetMapping("/assignment")
     public String assignment() {
