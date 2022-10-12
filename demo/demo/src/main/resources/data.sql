7499ad7695e71ed41193e3e46add11991fcc8243
RR <rebecca.rivera@awacademy.training>
2022-10-12 16:19:04 +0200

    SECURITY bilder

--- demo/demo/src/main/resources/data.sql
+++ demo/demo/src/main/resources/data.sql
@@ -23,7 +23,7 @@ INSERT INTO STUDENT (FIRST_NAME, LAST_NAME, EMAIL, LINKED_IN, PASSWORD) VALUES (
 INSERT INTO STUDENT (FIRST_NAME, LAST_NAME, EMAIL, PASSWORD) VALUES ('Carl Johan', 'Stefansson', 'carljohan@mail.com', '$2a$11$RCEkkIuaDpUlqn5PyzUjEOvwrNnLSJ4kQKke5QJV6nGaZQiXcoWPa');
 INSERT INTO STUDENT (FIRST_NAME, LAST_NAME, EMAIL, LINKED_IN, PASSWORD) VALUES ('Alvin', 'Skeppar', 'alvin@mail.com', 'https://se.linkedin.com/in/alvin-skeppar-5a87a1122/en' ,'$2a$11$RCEkkIuaDpUlqn5PyzUjEOvwrNnLSJ4kQKke5QJV6nGaZQiXcoWPa');
 INSERT INTO STUDENT (FIRST_NAME, LAST_NAME, EMAIL, LINKED_IN, PASSWORD) VALUES ('Markus', 'Turesson', 'markus@mail.com', 'https://se.linkedin.com/in/markus-turesson-sj%C3%B6man-095506115' ,'$2a$11$RCEkkIuaDpUlqn5PyzUjEOvwrNnLSJ4kQKke5QJV6nGaZQiXcoWPa');
-INSERT INTO STUDENT (FIRST_NAME, LAST_NAME, EMAIL, LINKED_IN, PASSWORD) VALUES ('Rebecca', 'Rivera', 'markus@mail.com', 'https://se.linkedin.com/in/rebecca-rivera-0926a9227' ,'$2a$11$RCEkkIuaDpUlqn5PyzUjEOvwrNnLSJ4kQKke5QJV6nGaZQiXcoWPa');
+INSERT INTO STUDENT (FIRST_NAME, LAST_NAME, EMAIL, LINKED_IN, PASSWORD) VALUES ('Rebecca', 'Rivera', 'rebecca@mail.com', 'https://se.linkedin.com/in/rebecca-rivera-0926a9227' ,'$2a$11$RCEkkIuaDpUlqn5PyzUjEOvwrNnLSJ4kQKke5QJV6nGaZQiXcoWPa');
 
 INSERT INTO TEACHER (FIRST_NAME, LAST_NAME, EMAIL, LINKED_IN, PASSWORD) VALUES ('Andreas', 'Olsson', 'olsson@mail.com', 'https://se.linkedin.com/in/andreasols/en?', '$2a$11$RCEkkIuaDpUlqn5PyzUjEOvwrNnLSJ4kQKke5QJV6nGaZQiXcoWPa');
 INSERT INTO TEACHER (FIRST_NAME, LAST_NAME, EMAIL, LINKED_IN, PASSWORD) VALUES ('Oscar', 'Olsson', 'Oscar.olsson@mail.com', 'https://se.linkedin.com/in/happybits?', '$2a$11$RCEkkIuaDpUlqn5PyzUjEOvwrNnLSJ4kQKke5QJV6nGaZQiXcoWPa');
